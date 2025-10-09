bbcbcd268b54744c8af60677419979d4978577b7
jaumegarijocamprubi-2009 <jaumegarijocamprubi@alumnes.iesllucmajor.org>
2025-10-09 12:42:11 +0200

    “Primera execució UP1Act5” hem creat dins la carpeta Activitats de la UP1 un nou directori que es diu Act5_ResumPersonal. Dins la carpeta he creat un fitxer Javascript anomenat JaumeGC_resum_personal.js. Li he demanat al usuari unes dades personals meves. El nom i llinatges i el meu color preferit l'he posat a un grup anomenat "Informació personal", en una linia he posat el meu esport preferit, però no he sapigut com posar tres numeros preferits dins una taula.

---
+++ Act5_ResumPersonal/JaumeGC_resum_personal.js
@@ -0,0 +1,6 @@
+"Informació personal"
+console.log(Nom i llinatges:"Jaume Garijo Camprubi")
+console.log(Color preferit:"Verd")
+
+console.log//L'esport que més "m'agrada:tennis"
+console.log("Tres numeros preferits":"2;6;15")
